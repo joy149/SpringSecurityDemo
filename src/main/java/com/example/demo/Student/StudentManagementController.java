ab7df70ce7ad653724495aa3bfc8b016e353d53f
joy149 <jbhwmck149@gmail.com>
2022-10-09 00:02:15 +0530

    Moving from ant matchers to preAuthorise each API

--- src/main/java/com/example/demo/Student/StudentManagementController.java
+++ src/main/java/com/example/demo/Student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.example.demo.Student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -16,11 +17,13 @@ public class StudentManagementController {
             new Student(3, "Ishu Bagra")));
 
     @GetMapping("/student")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         return studentList;
     }
 
     @PostMapping("/")
+    @PreAuthorize("hasAuthority('student:write')")
     public Student createStudent(@RequestBody Student student) {
          studentList.add(student);
         return student;
